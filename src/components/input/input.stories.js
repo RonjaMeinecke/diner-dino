09e5ec81041046769376ed26673d16b6337027be
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-01-26 16:50:10 +0100

    add input email

---
+++ src/components/input/input.stories.js
@@ -0,0 +1,9 @@
+import "./input.css";
+import inputemail from "./inputemail.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const input = () => inputemail;
