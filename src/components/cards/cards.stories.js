c3fa12fbf79232be61e8385c54da659c0f7ff563
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-01-27 15:06:58 +0100

    add grid system to cards

--- src/components/cards/cards.stories.js
+++ src/components/cards/cards.stories.js
@@ -2,6 +2,7 @@ import "./cards.css";
 import cardsMexican from "./mexican.html";
 import cardsKorean from "./korean.html";
 import cardsJapanese from "./japanese.html";
+import cardsContainer from "./card-container.html";
 
 export default {
   title: "Components/Cards",
@@ -11,3 +12,4 @@ export default {
 export const Mexican = () => cardsMexican;
 export const Korean = () => cardsKorean;
 export const Japanese = () => cardsJapanese;
+export const container = () => cardsContainer;
