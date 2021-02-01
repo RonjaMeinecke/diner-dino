fe55232ef7eadeab6a765fe6999004bd27954b22
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-02-01 16:59:27 +0100

    add JavaScript and button styling

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -1,10 +1,26 @@
+//   return `
+//         // <form class="form">
+//         //   <h2>Reset Password</h2>
+//         //   <p>Please enter your email to receive a link to create a new password via email</p>
+//         //   <input class="input-btn" placeholder="Your Email" />
+//         //   <input type="submit" />
+//         // </form>
+//       `;
+// }
 export function createRegisterForm() {
-  return `
-        <form class="form">
-          <h2>Reset Password</h2>
-          <p>Please enter your email to receive a link to create a new password via email</p>
-          <input placeholder="email" />
-          <input type="submit" />
-        </form>
-      `;
+  const form = document.createElement("form");
+  form.className = "form";
+  const button = document.createElement("button");
+  button.innerText = "Register";
+
+  function register() {
+    alert("Registered");
+  }
+  button.addEventListener("click", register);
+  const title = document.createElement("h2");
+  title.innerText = "Reset Password";
+
+  form.append(title, button);
+
+  return button;
 }
