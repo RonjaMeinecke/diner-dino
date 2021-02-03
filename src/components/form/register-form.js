f15aa96d98d564f2b4649bf0b462330d61afe685
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-02-03 10:05:15 +0100

    add lower caps and different font style

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -12,25 +12,25 @@ export function createRegisterForm() {
   form.className = "form";
 
   const button = document.createElement("button");
-  button.innerText = "Register";
+  button.innerText = "register";
   button.className = "btn";
 
   function register() {
-    alert("Registered");
+    alert("registered");
   }
 
   button.addEventListener("click", register);
 
   const title = document.createElement("h2");
-  title.innerText = "Reset Password";
+  title.innerText = "reset password";
 
   const text = document.createElement("p");
   text.innerText =
-    "Please enter your email to receive a link to create a new password via email";
+    "please enter your email to receive a link to create a new password via email";
 
   const input = document.createElement("input");
   input.type = "text";
-  input.placeholder = "Email";
+  input.placeholder = "email";
   input.className = "input";
 
   form.append(title, text, input, button);
